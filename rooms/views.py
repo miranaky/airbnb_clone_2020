47205d0a430df8fe8f488716d0d0f0d52614c8fb
miranaky <miranaky@gmail.com>
2020-09-15 21:46:23 -0500

    #12room detail view with function

--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,6 @@
-from django.utils import timezone
 from django.views.generic import ListView
+from django.http import Http404
+from django.shortcuts import render
 from . import models
 
 
@@ -12,3 +13,12 @@ class HomeView(ListView):
     paginate_orphans = 5
     ordering = "created"
     context_object_name = "rooms"
+
+
+def room_detail(request, pk):
+    try:
+        room = models.Room.objects.get(pk=pk)
+        return render(request, "rooms/detail.html", {"room": room})
+    except models.Room.DoesNotExist:
+        raise Http404()
+
