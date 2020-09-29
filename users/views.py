c56b450a65bfc8f4d0e781cd0e66cad4537ee879
miranaky <miranaky@gmail.com>
2020-09-29 00:27:53 -0500

    #15.1 signup with form

--- users/views.py
+++ users/views.py
@@ -3,7 +3,7 @@ from django.views.generic import FormView
 from django.urls import reverse_lazy
 from django.shortcuts import render, redirect, reverse
 from django.contrib.auth import authenticate, login, logout
-from . import forms
+from . import forms, models
 
 
 class LoginView(FormView):
@@ -20,17 +20,29 @@ class LoginView(FormView):
             login(self.request, user)
         return super().form_valid(form)
 
-    # def get(self, request):
-    #     form = forms.LoginForm(initial={"email": "smkang0321@gmail.com"})
-    #     return render(request, "users/login.html", {"form": form})
-
-    # def post(self, request):
-    #     form = forms.LoginForm(request.POST)
-    #     if form.is_valid():
-
-    #     return render(request, "users/login.html", {"form": form})
-
 
 def logout_view(request):
     logout(request)
     return redirect(reverse("core:home"))
+
+
+class SignupView(FormView):
+
+    template_name = "users/signup.html"
+    form_class = forms.SignUpForm
+    success_url = reverse_lazy("core:home")
+
+    initial = {
+        "first_name": "sungmuk",
+        "last_name": "kang",
+        "email": "smk@ng.com",
+    }
+
+    def form_valid(self, form):
+        form.save()
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
